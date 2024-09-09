32fa0fcfb9a6b434c04cc36e5afb8e5d31c4fed4
Simon Nilsson <simon.f.nilsson@me.com>
2024-09-09 12:25:28 +0200

    better

--- memory_manager.c
+++ memory_manager.c
@@ -1,21 +1,21 @@
 #include "memory_manager.h"
 
-unsigned char* block_starts_;
-unsigned char* block_ends_;
-void* start_;
+unsigned char *block_starts_;
+unsigned char *block_ends_;
+void *start_;
 size_t size_;
 
 /// @brief sets a bit to 1
 /// @param array
 /// @param index
-void set_bit(unsigned char* array, size_t index) {
+void set_bit(unsigned char *array, size_t index) {
     array[index / 8] |= (1 << (index % 8));
 }
 
 /// @brief sets a bit to 0
 /// @param array
 /// @param index
-void clear_bit(unsigned char* array, size_t index) {
+void clear_bit(unsigned char *array, size_t index) {
     array[index / 8] &= ~(1 << (index % 8));
 }
 
@@ -23,7 +23,7 @@ void clear_bit(unsigned char* array, size_t index) {
 /// @param array
 /// @param index
 /// @return
-bool get_bit(const unsigned char* array, size_t index) {
+bool get_bit(const unsigned char *array, size_t index) {
     return array[index / 8] & (1 << (index % 8));
 }
 
@@ -40,7 +40,7 @@ void mem_init(size_t size) {
 /// found
 /// @param size
 /// @return
-void* mem_alloc(size_t size) {
+void *mem_alloc(size_t size) {
     if (size == 0) return NULL;
     size_t current_empty_blocks = 0;
     bool empty = true;
@@ -58,18 +58,49 @@ void* mem_alloc(size_t size) {
     return NULL;
 }
 
+void *mem_alloc_bestfit(size_t size) {
+    if (size == 0) return NULL;
+    size_t current_empty_blocks = 0;
+    bool empty = true;
+    void *bestfit = NULL;
+    size_t smallest_size = -1;
+    size_t index = 0;
+    while (index < size_) {
+        if (get_bit(block_starts_, index) == 1) {
+            empty = false;
+            if (current_empty_blocks >= size &&
+                current_empty_blocks < smallest_size) {
+                bestfit = start_ + index - current_empty_blocks;
+                smallest_size = current_empty_blocks;
+            }
+            current_empty_blocks = 0;
+        }
+        if (empty) current_empty_blocks++;
+        if (get_bit(block_ends_, index) == 1) empty = true;
+        index++;
+    }
+    if (current_empty_blocks >= size && current_empty_blocks < smallest_size) {
+        bestfit = start_ + index - current_empty_blocks;
+        smallest_size = current_empty_blocks;
+    }
+    if (bestfit) {
+        set_bit(block_starts_, bestfit - start_);
+        set_bit(block_ends_, bestfit - start_ + size - 1);
+        return bestfit;
+    }
+    return NULL;
+}
+
 /// @brief Frees the memory block preventing memory leaks
 /// @param block
-void mem_free(void* block) {
-    if (block == NULL) return;
+void mem_free(void *block) {
     size_t block_index = block - start_;
-    if (block_index >= size_ || get_bit(block_starts_, block_index) == 0)
+    if (block_index >= size_ || get_bit(block_starts_, block_index) == 0 ||
+        block == NULL)
         return;
 
     clear_bit(block_starts_, block_index);
-    while (get_bit(block_ends_, block_index) == 0) {
-        block_index++;
-    }
+    while (get_bit(block_ends_, block_index) == 0) block_index++;
     clear_bit(block_ends_, block_index);
 }
 
@@ -78,46 +109,29 @@ void mem_free(void* block) {
 /// @param block
 /// @param size
 /// @return
-void* mem_resize(void* block, size_t size) {
+void *mem_resize(void *block, size_t size) {
     if (block == NULL) return mem_alloc(size);
-    size_t index = block - start_;
-    if (index >= size_ || size == 0) return NULL;
+    size_t start_index = block - start_;
+    if (start_index >= size_ || !get_bit(block_starts_, start_index)) return NULL;
 
-    size_t available_space = 1;
-    while (get_bit(block_ends_, index) == 0) {
-        available_space++;
-        index++;
-    }
-    size_t current_end_point = index;
-    size_t old_size = available_space;
+    size_t end_index = start_index;
+    while(!get_bit(block_ends_, end_index)) end_index++;
 
-    if (size < available_space) {
-        clear_bit(block_ends_, current_end_point);
-        set_bit(block_ends_, (block - start_) + size - 1);
-        return block;
-    }
-    if (old_size == size) {
-        return block;
-    }
+    mem_free(block);
+    if (size == 0) return NULL;
 
-    index++;
-    while (index < size_ && get_bit(block_starts_, index) == 0 &&
-           available_space < size) {
-        available_space++;
-        index++;
+    void* new_block = mem_alloc(size);
+    if(!new_block) {
+        set_bit(block_starts_, start_index);
+        set_bit(block_ends_, end_index);
+        return NULL;
     }
+    if(new_block == block) return block;
 
-    if (available_space == size) {
-        clear_bit(block_ends_, current_end_point);
-        set_bit(block_ends_, index);
-        return block;
-    } else {
-        void* new_block = mem_alloc(size);
-        if (new_block == NULL) return NULL;
-        memcpy(new_block, block, old_size);
-        mem_free(block);
-        return new_block;
-    }
+    size_t old_size = end_index - start_index + 1;
+    size_t min_size = (size < old_size) ? size : old_size;
+    memcpy(new_block, block, min_size);
+    return new_block;
 }
 
 /// @brief gives back the memory used by the memory manager
